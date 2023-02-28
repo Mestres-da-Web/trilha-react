27090a1148e21c77fa2cac9bbebc228439e2cf6d
Jancer Lima <contato.jancersl@gmail.com>
2023-02-27 23:39:33 -0300

    Revert "Aula 4"
    This reverts commit a87b7cc90278f69323702f20f24cb04b4abc15d9.

--- src/pages/SignUp/components/PosswordVisibilityButton/index.tsx
+++ src/pages/SignUp/components/PosswordVisibilityButton/index.tsx
@@ -6,15 +6,7 @@ interface PasswordVisibilityButtonProps {
 }
 
 function PasswordVisibilityButton({ visible }: PasswordVisibilityButtonProps) {
-  return (
-    <div>
-      {visible ? (
-        <EyeVisibleIcon />
-      ) : (
-        <EyeInvisibleIcon width={100} height={100} />
-      )}
-    </div>
-  );
+  return <div>{visible ? <EyeVisibleIcon /> : <EyeInvisibleIcon />}</div>;
 }
 
 export default PasswordVisibilityButton;
