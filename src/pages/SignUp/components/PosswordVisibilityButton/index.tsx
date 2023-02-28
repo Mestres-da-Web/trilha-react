a87b7cc90278f69323702f20f24cb04b4abc15d9
Jancer Lima <contato.jancersl@gmail.com>
2023-02-27 23:30:49 -0300

    Aula 4

--- src/pages/SignUp/components/PosswordVisibilityButton/index.tsx
+++ src/pages/SignUp/components/PosswordVisibilityButton/index.tsx
@@ -6,7 +6,15 @@ interface PasswordVisibilityButtonProps {
 }
 
 function PasswordVisibilityButton({ visible }: PasswordVisibilityButtonProps) {
-  return <div>{visible ? <EyeVisibleIcon /> : <EyeInvisibleIcon />}</div>;
+  return (
+    <div>
+      {visible ? (
+        <EyeVisibleIcon />
+      ) : (
+        <EyeInvisibleIcon width={100} height={100} />
+      )}
+    </div>
+  );
 }
 
 export default PasswordVisibilityButton;
