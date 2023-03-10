99d8c8044168c312a7275c29d4746367cf2d95ae
Jancer Lima <contato.jancersl@gmail.com>
2023-03-09 21:24:19 -0300

    4 - Aula 1

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import Input from "../../components/Input";
 import LargeButton from "../../components/LargeButton";
-import EffectExample from "../Examples/EffectExample";
+import Illustration from "../../assets/Images/Illustration.png";
+import MWLogo from "../../assets/Images/MWLogo.png";
+import styles from "./styles.module.css";
 
 function SignUp() {
-  const [showComponent, setShowComponent] = useState(false);
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -16,35 +16,33 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <h1>State e lifecycle</h1>
+    <div className={styles.container}>
+      <div className={styles.leftColumn}>
+        <img className={styles.logo} src={MWLogo} alt="Logo" />
+        <img src={Illustration} alt="Ilustração" />
+      </div>
 
-      {/* <form onSubmit={handleSubmit}>
-        <Input name="name" title="Nome" placeholder="Insira seu nome" />
-        <Input
-          name="phone"
-          title="Telefone"
-          placeholder="Insira seu telefone"
-        />
-        <Input name="email" title="E-mail" placeholder="Insira seu e-mail" />
-        <Input name="password" title="Senha" placeholder="Insira sua senha" />
-        <Input
-          name="passwordConfirmation"
-          title="Confirmar senha"
-          placeholder="Insira sua senha novamente"
-        />
-        <LargeButton title="Fazer cadastro" type="submit" /> <br />
-      </form> */}
+      <div className={styles.rightColumn}>
+        <h1 className={styles.title}>Seja bem vindo!</h1>
+        <p className={styles.description}>Faça o cadastro para continuar</p>
 
-      {showComponent ? <EffectExample /> : null}
-      <button
-        type="button"
-        onClick={() => {
-          setShowComponent(!showComponent);
-        }}
-      >
-        {showComponent ? "Remover componente" : "Adicionar componente"}
-      </button>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Input name="name" title="Nome" placeholder="Insira seu nome" />
+          <Input
+            name="phone"
+            title="Telefone"
+            placeholder="Insira seu telefone"
+          />
+          <Input name="email" title="E-mail" placeholder="Insira seu e-mail" />
+          <Input name="password" title="Senha" placeholder="Insira sua senha" />
+          <Input
+            name="passwordConfirmation"
+            title="Confirmar senha"
+            placeholder="Insira sua senha novamente"
+          />
+          <LargeButton title="Fazer cadastro" type="submit" /> <br />
+        </form>
+      </div>
     </div>
   );
 }
