54991794fe3693508ba216784bc1925f921090ff
Jancer Lima <contato.jancersl@gmail.com>
2023-03-11 23:29:33 -0300

    4 - Aula 2

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,24 +1,54 @@
+import { useState } from "react";
 import { ChangeEvent } from "react";
+import EyeInvisibleIcon from "../../assets/EyeInvisibleIcon";
+import EyeVisibleIcon from "../../assets/EyeVisibleIcon";
 import styles from "./styles.module.css";
 
 interface InputProps {
   title: string;
   name: string;
   placeholder: string;
+  password?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ title, name, placeholder, onChange }: InputProps) {
+function Input({
+  title,
+  name,
+  placeholder,
+  password = false,
+  onChange,
+}: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className={styles.container}>
       <label className={styles.label}>{title} </label>
+
       <input
         className={styles.input}
-        type="text"
+        type={password ? (showPassword ? "text" : "password") : "text"}
         name={name}
         placeholder={placeholder}
         onChange={onChange}
       />
+      {password ? (
+        showPassword ? (
+          <EyeInvisibleIcon
+            className={styles.icon}
+            onClick={handleShowPassword}
+          />
+        ) : (
+          <EyeVisibleIcon
+            className={styles.icon}
+            onClick={handleShowPassword}
+          />
+        )
+      ) : null}
     </div>
   );
 }
