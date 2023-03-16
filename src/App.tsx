c2b25607938a91bc7c9d574d9a4cff65bd7ee16a
Jancer Lima <contato.jancersl@gmail.com>
2023-03-15 22:05:41 -0300

    5 - Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,19 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/forgot" element={<ForgotPassword />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="teste" element={<Layout />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="forgot" element={<ForgotPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
