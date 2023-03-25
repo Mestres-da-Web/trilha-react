408ecaa9983808cf7cba16bcc7e1f524255f5672
Jancer Lima <contato.jancersl@gmail.com>
2023-03-25 19:58:58 -0300

    Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,18 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import NotFound from "./pages/NotFound";
-
-import ContextExample, { ContextProvider } from "./pages/Examples/useContext";
+import UseRefExample from "./pages/Examples/useRef";
 
 function App() {
   return (
-    <ContextProvider>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="hooks" element={<ContextExample />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="forgot" element={<ForgotPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </ContextProvider>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="hooks" element={<UseRefExample />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="forgot" element={<ForgotPassword />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
