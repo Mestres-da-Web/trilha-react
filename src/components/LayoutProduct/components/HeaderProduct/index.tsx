8c3b105b331d84d8e0da5012dffcc950121a9eb5
Jancer Lima <contato.jancersl@gmail.com>
2023-07-29 01:03:07 -0300

    7 - Aula 1

---
+++ src/components/LayoutProduct/components/HeaderProduct/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+function HeaderProduct() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello from header</h1>
+    </div>
+  );
+}
+
+export default HeaderProduct;
---
+++ src/components/LayoutProduct/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello from left</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
