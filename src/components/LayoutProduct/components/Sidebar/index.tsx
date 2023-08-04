3df333351f62a7f9d64d60c9d211458f12d7da16
Jancer Lima <contato.jancersl@gmail.com>
2023-08-04 00:15:24 -0300

    Product - Aula 2

--- src/components/LayoutProduct/components/Sidebar/index.tsx
+++ src/components/LayoutProduct/components/Sidebar/index.tsx
@@ -1,9 +1,21 @@
 import styles from "./styles.module.css";
 
+import MWLogo from "../../../../assets/Icons/mw-icon.svg";
+import DetailsLogo from "../../../../assets/Icons/list-icon.svg";
+
 function Sidebar() {
   return (
     <div className={styles.container}>
-      <h1>Hello from left</h1>
+      <nav>
+        <ul className={styles.navUl}>
+          <li>
+            <img src={MWLogo} alt="Logo mestres" />
+          </li>
+          <li className={styles.secondIcon}>
+            <img src={DetailsLogo} alt="Icone de detalhes" />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
