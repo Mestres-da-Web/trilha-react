308c7ca39682468019629acc1624b548227b7246
Jancer Lima <contato.jancersl@gmail.com>
2023-08-30 22:34:43 -0300

    Product - Aula 7

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import LayoutProduct from "./components/LayoutProduct";
 import "./global.styles.css";
 
 import Products from "./pages/Products";
+import { GlobalContextProvider } from "./context/GlobalContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LayoutProduct />}>
-        <Route path="product" element={<Products />} />
-      </Route>
-    </Routes>
+    <GlobalContextProvider>
+      <Routes>
+        <Route path="/" element={<LayoutProduct />}>
+          <Route path="product" element={<Products />} />
+        </Route>
+      </Routes>
+    </GlobalContextProvider>
   );
 }
 
--- src/pages/Products/components/ProductSubHeader/components/Button/index.tsx
+++ src/pages/Products/components/ProductSubHeader/components/Button/index.tsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../../../../../context/GlobalContext";
+
 interface ButtonProps {
   onAddProduct(): void;
 }
 
 function Button({ onAddProduct }: ButtonProps) {
+  const { value } = useContext(GlobalContext);
   return (
     <div>
       <button>Adicionar</button>
+      <h1>Value: {value}</h1>
     </div>
   );
 }
