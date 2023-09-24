a3e029ba373daf0ef475521ad9b0cb7c35f6066e
Jancer Lima <contato.jancersl@gmail.com>
2023-09-24 09:07:22 -0300

    Product - Aula 16

--- src/components/Button/ButtonRoot.tsx
+++ src/components/Button/ButtonRoot.tsx
@@ -3,11 +3,21 @@ import styles from "./styles.module.css";
 interface ButtonRootProps {
   children: React.ReactNode;
   onClick?: () => void;
+  type?: "default" | "error";
 }
 
-const ButtonRoot = ({ children, onClick }: ButtonRootProps) => {
+const ButtonRoot = ({
+  children,
+  onClick,
+  type = "default",
+}: ButtonRootProps) => {
   return (
-    <button className={styles.root} onClick={onClick}>
+    <button
+      className={`${styles.root} ${
+        type === "default" ? styles.defaultColor : styles.errorColor
+      }`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
